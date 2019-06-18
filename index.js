8cfba66737f3b66fb7c61c39eb9e97e97528a349
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2019-06-18 14:37:13 -0400

    generate index from examples (vs hardcode)

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 (function() {
+  const { transducers: tx, hdom } = thi.ng;
+  //  const { MELD_EXAMPLES } = window;
+  console.log(`MELD_examples`, MELD_EXAMPLES);
+
+  const render_examples = (_, items) => [
+    "ul",
+    tx.map(
+      item => ["li", ["a", { href: `/model.html?${item.name}` }, item.label]],
+      items
+    )
+  ];
+  hdom.renderOnce(() => [render_examples, MELD_EXAMPLES], { root: "examples" });
+
   // except I think this is not needed now
   // HACK: work around unwanted scrolling of page when focusing textarea.
   document.body.addEventListener(
