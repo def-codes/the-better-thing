58e7f2a5c184556a39bcfed090dba4f77037b328
Gavin Cannizzaro <gavin@gavinpc.com>
2019-06-24 04:00:20 -0400

    FIX: prevent unsubscription in dataflow node when changing transducer downstream

--- subscription-driver.js
+++ subscription-driver.js
@@ -10,13 +10,24 @@
     let current;
     let current_xform;
 
+    const make_merge = opts => {
+      const merge = rs.merge({ id: `${id} merge`, close: false, ...opts });
+      // DUMMY subscription.  This prevents the merge from unsubscribing its
+      // sources when it's being swapped out in the meta-stream.  If we had to
+      // truly tear down this node (e.g. for non-monotonic dataflows), we'd want
+      // to unsubscribe this as well.  A way to opt out of this behavior is
+      // under consideration: https://github.com/thi-ng/umbrella/issues/74
+      merge.subscribe({});
+      return merge;
+    };
+
     // Returning the same stream as the current one (which shouldn't happen),
     // will break because it will first unsubscribe it.
     const meta = rs.metaStream(
       sub => (current === sub ? null : (current = sub)),
       `${id} metamerge`
     );
-    meta.next(rs.merge({ id: `${id} merge`, close: false }));
+    meta.next(make_merge());
 
     return Object.assign(meta, {
       add: (...args) => current.add(...args),
@@ -24,9 +35,8 @@
         // Don't rebuild if transform hasn't really changed, in case that's not
         // prevented upstream.
         if (xform !== current_xform)
-          meta.next(
-            rs.merge({ src: [...current.sources.keys()], xform, close: false })
-          );
+          meta.next(make_merge({ src: [...current.sources.keys()], xform }));
+
         current_xform = xform;
       }
     });
