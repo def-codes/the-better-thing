bdfd47602a06043ef2f05d6a235568c622233b0a
Gavin Cannizzaro <gavin@gavinpc.com>
2019-06-30 12:10:00 -0400

    fix build errors in rdf-expressions package

--- src/expression-reader/index.ts
+++ src/expression-reader/index.ts
@@ -1,3 +1,4 @@
+export * from "./api";
 export * from "./reader";
 export * from "./scanner";
 export * from "./serializer";
--- src/rdf-expressions/turtle-expand.ts
+++ src/rdf-expressions/turtle-expand.ts
@@ -1,11 +1,31 @@
 // should mint be in context?
+import { TermNode, LiteralNode } from "@def.codes/expression-reader";
+
+// Yes, we're still using rstream-query-style triples, but with RDF.js terms.
+type MintedNode = { term: { minted: number } };
+type PseudoTerm = TermNode | LiteralNode | MintedNode;
+type PseudoTriple = [PseudoTerm, PseudoTerm, PseudoTerm];
+
+// You should always have a subject if you have a predicate.  There's not a good
+// way to say that right now.
+type ReaderContext = { subject?: TermNode | MintedNode; predicate?: TermNode };
+
+type BacktrackableResult = readonly PseudoTriple[] | { error: Error };
+
+interface ScannerFunction {
+  (expr, context: ReaderContext): IterableIterator<PseudoTriple>;
+}
 
 let count = 0;
 const mint = (function() {
-  return () => ({ term: { minted: count++ } });
+  return (): MintedNode => ({ term: { minted: count++ } });
 })();
 
-function scan_as(fn, expr, context = {}): readonly any[] | { error: Error } {
+function scan_as(
+  fn: ScannerFunction,
+  expr,
+  context: ReaderContext = {}
+): BacktrackableResult {
   try {
     return [...fn(expr, context)];
   } catch (error) {
@@ -13,11 +33,15 @@ function scan_as(fn, expr, context = {}): readonly any[] | { error: Error } {
   }
 }
 
-function scan_as_list(fn, exprs, context = {}) {
+function scan_as_list(
+  fn: ScannerFunction,
+  exprs,
+  context: ReaderContext = {}
+): BacktrackableResult {
   const out = [];
   for (const expr of exprs) {
     const facts = scan_as(fn, expr, context);
-    if (facts.error) return facts;
+    if ("error" in facts) return facts;
     out.push(...facts);
   }
   return out;
@@ -53,14 +77,14 @@ export function* expecting_statement(expr) {
       const po_facts = scan_as_list(expecting_predicate_object, args, {
         subject
       });
-      if (!po_facts.error) {
+      if (!("error" in po_facts)) {
         yield* po_facts;
         return;
       }
 
       // Predicate + object list: blank node
       const o_facts = scan_as_list(expecting_object, args);
-      if (!o_facts.error) {
+      if (!("error" in o_facts)) {
         yield* o_facts;
         return;
       }
@@ -84,7 +108,7 @@ export function* expecting_statement(expr) {
       subject,
       predicate
     });
-    if (!o_list_facts.error) {
+    if (!("error" in o_list_facts)) {
       yield* o_list_facts;
       return;
     }
@@ -101,7 +125,10 @@ export function* expecting_statement(expr) {
 // expr means an array of elements (term, literal, or array)
 // exprs means an array of expr's
 
-function* expecting_predicate_object(expr, { subject }) {
+const expecting_predicate_object: ScannerFunction = function*(
+  expr,
+  { subject }
+) {
   if (expr.length !== 2) {
     throw "Expecting predicate object: invalid arity";
   }
@@ -123,16 +150,19 @@ function* expecting_predicate_object(expr, { subject }) {
       subject,
       predicate
     });
-    if (!o_list_facts.error) {
+    if (!("error" in o_list_facts)) {
       yield* o_list_facts;
       return;
     }
   }
 
   throw "Expecting predicate object: exhausted";
-}
+};
 
-function* expecting_object(expr, { subject, predicate }) {
+const expecting_object: ScannerFunction = function*(
+  expr,
+  { subject, predicate }
+) {
   if ("literal" in expr) {
     yield [subject, predicate, expr];
     return;
@@ -158,7 +188,7 @@ function* expecting_object(expr, { subject, predicate }) {
     const object_facts = scan_as(expecting_predicate_object, expr, {
       subject: mint()
     });
-    if (!object_facts.error) {
+    if (!("error" in object_facts)) {
       // In addition to the collected facts, yield a fact (for *this* position)
       // based on head of result where object is the (presumably minted) id of
       // the implicit object.
@@ -172,4 +202,4 @@ function* expecting_object(expr, { subject, predicate }) {
   }
 
   throw "Expecting object: exhausted";
-}
+};
