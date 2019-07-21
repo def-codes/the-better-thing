ae8a8247d4085c8d23f0026d431040b6867031f2
Gavin Cannizzaro <=>
2019-07-20 22:16:33 -0400

    playing with drop targets, this killed firefox

--- portable.js
+++ portable.js
@@ -13,16 +13,47 @@ requirejs(["@thi.ng/transducers", "@def.codes/meld-demo"], tx => {
     ..."end enter exit leave over start".split(" ").map(name => `drag${name}`),
   ];
 
-  const log = name =>
-    function(event) {
-      console.log(name);
-    };
-
   const closest_draggable = ele => {
     do {
       if (ele.draggable) return ele;
     } while ((ele = ele.parentNode));
   };
+
+  const log = name =>
+    function(event) {
+      // console.orig.log(name);
+      if (name === "dragstart") {
+        const draggable = closest_draggable(event.originalTarget);
+        if (draggable) {
+          event.dataTransfer.effectAllowed = "all";
+
+          // worse than default
+          // event.dataTransfer.setDragImage(draggable, 0, 0);
+
+          // What is this supposed to be?
+          event.dataTransfer.setData("text/plain", "#"); // draggable.innerText);
+        } else console.orig.warn("expected draggable for dragstart", event);
+      } else if (name === "dragenter") {
+        console.log(`enter`, event.originalTarget);
+        if (event.originalTarget.nodeType === 1) {
+          console.log(`..element`, event.originalTarget);
+
+          event.preventDefault();
+          event.originalTarget.classList.add("DropTarget");
+        }
+      } else if (name === "dragover") {
+        // Only matters if same was done during dragenter
+        console.log(`move`, event.originalTarget);
+
+        event.dataTransfer.dropEffect = "move";
+        event.preventDefault();
+      } else if (name === "dragleave") {
+        console.orig.log("drag leave", event.originalTarget);
+        if (event.originalTarget.nodeType === 1) {
+          event.originalTarget.classList.remove("DropTarget");
+        }
+      } else console.log(name);
+    };
 
   // feature detection, blah
   document.body.addEventListener(
@@ -40,4 +71,7 @@ requirejs(["@thi.ng/transducers", "@def.codes/meld-demo"], tx => {
 
   for (const name of DRAG_EVENT_NAMES)
     document.body.addEventListener(name, log(name));
+
+  // A drag operation is a dragstart, a succession of other drag events,
+  // ending with a dragend[/exit?] or drop event
 });
