d9baeed1b089e8f7708cdcd9face75e0f2e641aa
Gavin Cannizzaro <=>
2019-08-06 15:55:42 -0400

    amd loader: additional refactoring & cleanup

--- src/amd-loader/AsyncMap.ts
+++ src/amd-loader/AsyncMap.ts
@@ -1,30 +1,23 @@
-/** General-purpose async registry for coordinating requests with things.  Lets
- *  consumers await items until providers register them by name. */
-
 interface PendingItem<T> {
   promise: Promise<T>;
   // Same as first argument (`resolve`) to `Promise` executor
   resolve: (value?: T | PromiseLike<T>) => void;
 }
 
+/** General-purpose async registry for coordinating requests with things.  Lets
+ *  consumers await items until providers register them by name. */
 export class AsyncMap<K, V> extends Map<K, V | Promise<V>> {
   readonly pending = new Map<K, PendingItem<V>>();
 
-  protected promise_for(key: K): Promise<V> {
+  async get(key: K) {
+    if (this.has(key)) return super.get(key);
     if (this.pending.has(key)) return this.pending.get(key).promise;
 
     const promise = new Promise<V>(resolve => {
       this.pending.set(key, { promise, resolve });
-    }).then(value => {
-      this.pending.delete(key);
-      return value;
-    });
+    }).finally(() => this.pending.delete(key));
 
     return promise;
-  }
-
-  async get(key: K) {
-    return this.has(key) ? super.get(key) : this.promise_for(key);
   }
 
   set(key: K, value: V | Promise<V>) {
