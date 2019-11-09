7924eed8e781664a6f78e6713118314d6fc61768
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2019-11-09 18:05:53 -0500

    graphviz-format: add attribute metadata and use it to refine types

--- packages/graphviz-format/api.ts
+++ packages/graphviz-format/api.ts
@@ -1,6 +1,10 @@
 // This is not isomorphic to the Dot language because it hoists all general
 // attributes, whose declaration order, at least insofar as they are interleaved
 // with other statements, does not matter.  Also doesn't include HTML labels.
+
+import { ATTRIBUTES_METADATA } from "./attributes-metadata";
+
+// Not currently used
 export type CompassPoint =
   | "n"
   | "ne"
@@ -53,9 +57,6 @@ export interface Edge {
   attributes?: EdgeAttributes;
 }
 
-// I may or may not spec out the values for these
-// https://www.graphviz.org/doc/info/attrs.html
-
 export interface KeyedRecordField {
   key: string | number;
   // You cannot key a set of fields, i.e. `<id> { p | q }` doesn't work.
@@ -73,24 +74,124 @@ export type RecordLabel = string | number | KeyedRecordField | RecordFields;
 
 export type NodeLabel = string | number | RecordLabel; // Or HtmlLikeLabel (TBD)
 
-export interface GraphAttributes {
-  [key: string]: string | number;
+// TODO: special serialize for the "List" types
+// TODO: finish adding notes and specializing types
+// TODO: include "string" on all types? to provide directly
+// TODO: can support completion for strings where only *some* values are known?
+export interface AttributeTypes {
+  addDouble: number;
+
+  addPoint: string | AttributeTypes["point"];
+
+  /** box crow curve diamond dot icurve inv none normal tee vee */
+  arrowType: string;
+
+  bool: boolean | "true" | "false";
+
+  clusterMode: "local" | "global" | "none";
+
+  color: string;
+
+  /** A colon-separated list of weighted color values: WC(:WC)* where each WC
+   * has the form C(;F)? with C a color value and the optional F a
+   * floating-point number, 0 ≤ F ≤ 1. The sum of the floating-point numbers in
+   * a colorList must sum to at most 1. */
+  colorList: string | AttributeTypes["color"][];
+
+  dirType: "forward" | "back" | "both" | "none";
+
+  double: number;
+
+  /** A colon-separated list of doubles: "%f(:%f)*" where each %f is a
+   * double.  */
+  doubleList: string | AttributeTypes["double"][];
+
+  escString: string;
+
+  int: number;
+
+  layerList: string | string[];
+
+  layerRange: string | string[];
+
+  // Technically NodeLabel should only apply for nodes
+  lblString: string | NodeLabel; // Or HtmlLikeLabel (TBD)
+
+  outputMode: "breadthfirst" | "nodesfirst" | "edgesfirst";
+
+  /** "node", "clust" , "graph" , "array(_flags)?(%d)?"  */
+  packMode: string;
+
+  pagedir: "BL" | "BR" | "TL" | "TR" | "RB" | "RT" | "LB" | "LT";
+
+  /** "%f,%f('!')?" representing the point (x,y). The optional '!' indicates the
+   * node position should not change (input-only).
+   *
+   * If dim is 3, point may also have the format "%f,%f,%f('!')?" to represent
+   * the point (x,y,z).  */
+  point: string;
+
+  pointList: string; // | AttributeTypes['point'][];
+
+  portPos: string; // CompassPoint but also composites
+
+  quadType: "normal" | "fast" | "none";
+
+  rankType: "same" | "min" | "source" | "max" | "sink";
+
+  rankdir: "TB" | "LR" | "BT" | "RL";
+
+  rect: string;
+
+  shape: string;
+
+  smoothType:
+    | "none"
+    | "avg_dist"
+    | "graph_dist"
+    | "power_dist"
+    | "rng"
+    | "spring"
+    | "triangle";
+
+  splineType: string;
+
+  startType: string;
+
+  string: string;
+
+  style: string;
+
+  viewPort: string;
 }
 
-export interface NodeAttributes {
-  label: NodeLabel;
-  // Allowing `object` here so that label doesn't contradict it.  Ultimately,
-  // I'll probably define the recognized values outright.
-  [key: string]: string | number | object;
-}
+type Meta = typeof ATTRIBUTES_METADATA;
 
-export interface EdgeAttributes {
-  [key: string]: string | number;
-}
+type ArrayItemType<T> = T extends ReadonlyArray<infer E> ? E : never;
 
-export interface SubgraphAttributes {
-  [key: string]: string | number;
-}
+// Sub-conditional is a trick to make type distribute over key
+type FilterAttributes<
+  T,
+  K extends keyof Meta = keyof Meta
+> = K extends (T extends ArrayItemType<Meta[K]["used_by"]>
+? K
+: never)
+  ? K
+  : never;
+
+type UsedBy<T> = {
+  // Make all optional
+  [K in FilterAttributes<T>]?: AttributeTypes[ArrayItemType<Meta[K]["types"]>];
+  // After all that, use this escape hatch so types are not strict.
+  // The special attribute types are really a guide for interactive use.
+  // TS: Shouldn't be `object` but test cases blow up compiler without it.
+} & { [key: string]: string | number | object | any };
+
+// Neat, but you don't get (JS) documentation on the resulting types.
+export type NodeAttributes = UsedBy<"N">;
+export type EdgeAttributes = UsedBy<"E">;
+export type GraphAttributes = UsedBy<"G">;
+export type SubgraphAttributes = UsedBy<"C">;
 
 export interface SerializeOptions {
   directed?: boolean;
