f2f05e42e86f616a0d772083f920d4c01d4f9508
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2019-11-19 00:32:20 -0500

    use existing expression reader for JS playground

--- packages/meld/index.ts
+++ packages/meld/index.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 import * as path from "path";
-import { dot_updater } from "@def.codes/node-web-presentation";
+import { read } from "@def.codes/expression-reader";
 import { object_graph_to_dot } from "@def.codes/graphviz-format";
-import { make_interpreter } from "./interpreter";
+import { dot_updater } from "@def.codes/node-web-presentation";
 import { filesystem_watcher_source } from "@def.codes/process-trees";
 import * as rs from "@thi.ng/rstream";
 
@@ -15,13 +15,21 @@ function main() {
     process.exit(0);
   }
 
-  const interpreter = make_interpreter(filename);
   const updater = dot_updater();
 
   function do_interpret() {
     const code = fs.readFileSync(filename, "utf8");
-    const things = interpreter.interpret(code);
-    updater.go(object_graph_to_dot(things));
+
+    let statements;
+    try {
+      // We *could* use a VM with a Proxy as context, but expression reader
+      // isn't factored for that and this works fine.
+      statements = read(code);
+    } catch (error) {
+      statements = { error, when: "reading-code" };
+    }
+
+    updater.go(object_graph_to_dot(statements));
   }
 
   // Watch
--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,9 @@
-hello.world;
-once.there.was;
+// let's build us a dataflow!
+
+alice = timer(1000);
+
+bob.has.a.great.big.philosophy;
+
+sdf;
+
+// a = things(and(stuff));
