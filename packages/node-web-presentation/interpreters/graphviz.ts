22eca694e33e81a58c1f4d0f12858190a485fd8d
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2019-12-03 00:09:27 -0500

    move a bunch of ramblings into notes dir

---
+++ packages/node-web-presentation/interpreters/graphviz.ts
@@ -0,0 +1,6 @@
+// Graphviz interpreter.  Bear with me.
+export const graphviz_interpreter = () => {
+  // la la la
+  // is this about the whole launching a browser thing?
+  // or just somehow getting the graphviz output in front of the person
+};
