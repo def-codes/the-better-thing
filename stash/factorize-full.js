d7318ee9114d7bd317d68026dd262d6e7210fd0a
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2019-12-06 14:33:13 -0500

    move factorize tests into modules

--- stash/factorize-full.js
+++ stash/factorize-full.js
@@ -5,21 +5,63 @@ const { display } = require("@def.codes/node-web-presentation");
 const dot = require("@def.codes/graphviz-format");
 const { pipeline } = require("./lib/pipeline");
 const { prefix_keys } = require("./lib/clustering");
-const { visit_all_factors, visit_prime_factors } = require("./lib/factorize");
 
 const input = { something: ["non", "trivial"] };
+
+const shallow_clone = o => (Array.isArray(o) ? [...o] : { ...o });
+const deep_clone = o => JSON.parse(JSON.stringify(o));
 
 const EMPTY_ARRAY = [];
 const DEFAULT_MOVES_FROM = () => EMPTY_ARRAY;
 const DEFAULT_VALUE_OF = () => {};
 
-function* traverse1(starts, spec, state) {
+function* prime_factors(n) {
+  if (n < 3) return;
+  let count = 0,
+    m = n,
+    i = 2,
+    h = Math.ceil(n / 2);
+  do {
+    while (m % i === 0) {
+      count++;
+      yield i;
+      m /= i;
+    }
+    i++;
+  } while (i <= h);
+  // prime... but there's got to be a better way
+  if (count === 0) yield n;
+}
+
+function* all_factors(n) {
+  let count = 0,
+    m = n,
+    i = 2,
+    h = Math.ceil(n / 2);
+  do {
+    while (m % i === 0) {
+      count++;
+      // yield [i, n / i];
+      // yield [n / i, i];
+      yield [m, n / m];
+      yield [n / m, m];
+      m /= i;
+    }
+    i++;
+  } while (i <= h);
+  // if (count === 0) yield [n, 1];
+}
+
+const factorize = n => [...prime_factors(n)];
+const all_factorize = n => [...all_factors(n)];
+
+function* traverse1(starts, factor_spec, state) {
   state = state || {};
-  spec = spec || {};
+  factor_spec = factor_spec || {};
   const queue = state.queue || (state.queue = []);
   const visited = state.visited || (state.visited = new Set());
-  const moves_from = spec.moves_from || DEFAULT_MOVES_FROM;
-  const value_of = spec.value_of || DEFAULT_VALUE_OF;
+  const moves_from = factor_spec.moves_from || DEFAULT_MOVES_FROM;
+  const value_of = factor_spec.value_of || DEFAULT_VALUE_OF;
   for (const start of starts) queue.push(start);
 
   while (has_items(queue)) {
@@ -36,10 +78,11 @@ function* traverse1(starts, spec, state) {
 }
 
 // this is a sequence you could iterate through
-const N = 1184;
+const N = 1002;
+const prime_factor_spec = { moves_from: n => factorize(n).map(n => [n]) };
+const factor_spec = { moves_from: all_factorize };
 
-const traversal_spec = visit_all_factors;
-const traversed = [...traverse1([N], traversal_spec)];
+const traversed = [...traverse1([N], factor_spec)];
 // const constructed = from_facts(tx.map(prefix_keys("NN"), traversed));
 const constructed = from_facts(traversed);
 
