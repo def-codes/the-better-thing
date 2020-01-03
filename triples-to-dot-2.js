e9de9d73ae10776ac18ec5addb6ec1284951f3a0
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-03 09:08:09 -0500

    create initial test cases for simple entailment and "merged" view of individual cases

--- triples-to-dot-2.js
+++ triples-to-dot-2.js
@@ -1,52 +1,39 @@
+const tx = require("@thi.ng/transducers");
 const { DOT, TYPES } = require("@def.codes/graphviz-format");
 const { NODE, EDGE } = TYPES;
-const { RDFTripleStore } = require("@def.codes/rstream-query-rdf");
-const { make_identity_factory } = require("@def.codes/rdf-data-model");
-const { traverse } = require("@def.codes/graphs");
-const {
-  make_object_graph_traversal_spec,
-} = require("@def.codes/node-web-presentation");
-
-const factory = make_identity_factory();
-const { namedNode: n, blankNode: b, literal: l, variable: v } = factory;
-
-const { some_object_graph } = require("./lib/test-object-graph");
-const { some_ast } = require("./lib/some-ast");
-const { evaluate_cases } = require("./lib/evaluate-cases");
-const {
-  simple_records,
-  symmetric_property_with_bnode,
-} = require("./lib/rdf-js-examples");
-// const trips = [...rdf_js_traversal(evaluate_cases)];
-const trips = symmetric_property_with_bnode;
-// console.log(`facts`, trips);
-const thing = trips;
-
-const store = new RDFTripleStore();
-for (const trip of trips) store.add(trip);
-// store.add([n("dolphins"), n(`${DOT}shape`), l("circle")]);
-// store.add([b("b2536"), n(`${DOT}color`), l("red")]);
-
+const { RDFTripleStore, factory } = require("@def.codes/rstream-query-rdf");
 const {
   triples_to_dot_description,
 } = require("./lib/triples-to-dot-description");
-const name = "Bob";
-const store2 = new RDFTripleStore();
-store2.into([
-  ...triples_to_dot_description(store),
-  [n(name), n(`${DOT}style`), l("filled")],
-  [n(name), n(`${DOT}color`), l("red")],
-  [n(name), n(`${DOT}shape`), l("circle")],
-]);
-
 const { sync_query } = require("@def.codes/rstream-query-rdf");
-// const results = sync_query(store, [[n("Carol"), n("loves"), v("lover")]]);
-// console.log(`results`, results);
+const { dot_interpret_rdf_store } = require("./lib/dot-interpret-rdf-store");
+const { rdfjs_store_to_dot_statements } = require("./lib/rdf-js-to-dot");
+
+const { namedNode: n, blankNode: b, literal: l, variable: v } = factory;
+
+const TYPE = n("rdf:type");
+
+function mark_node(store, node) {
+  store.into([
+    [node, n(`${DOT}style`), l("filled")],
+    [node, n(`${DOT}color`), l("red")],
+    [node, n(`${DOT}shape`), l("circle")],
+  ]);
+}
+
+function mark_edge(store, from, to) {
+  const dot_edges = find_dot_edges(store, from, to);
+  if (dot_edges) {
+    const [dot_edge] = dot_edges;
+    console.log(`dot_edge`, dot_edge);
+    if (dot_edge) store.into([[dot_edge, n(`${DOT}color`), l("green")]]);
+  }
+}
 
 const find_dot_edges = (store, from, to) =>
   Array.from(
     sync_query(store, [
-      [v("edge"), n("rdf:type"), n(EDGE)],
+      [v("edge"), TYPE, n(EDGE)],
       [v("edge"), n(`${DOT}from`), from],
       [v("edge"), n(`${DOT}to`), to],
     ]) || [],
@@ -54,46 +41,84 @@ const find_dot_edges = (store, from, to) =>
   );
 
 const is_blank_node = term => term.termType === "BlankNode";
-
-const tx = require("@thi.ng/transducers");
 const find_blank_nodes = store =>
   tx.iterator(
     tx.filter(is_blank_node),
     tx.concat(store.indexS.keys(), store.indexO.keys())
   );
 
-function* find_blank_nodes_0(store) {
-  for (const key of store.indexS.keys()) if (is_blank_node(key)) yield key;
-  for (const key of store.indexO.keys()) if (is_blank_node(key)) yield key;
+// all this is a hack based on a defunct approach to dot mapping
+function mark_bnodes(store) {
+  store.into(
+    tx.iterator(
+      tx.comp(
+        // edges are blank nodes
+        tx.filter(node => !store.has([node, TYPE, n(EDGE)])),
+        tx.mapcat(bn => [
+          [bn, n(`${DOT}shape`), l("square")],
+          [bn, n(`${DOT}label`), l("")],
+          [bn, n(`${DOT}width`), l(0.1)],
+          [bn, n(`${DOT}style`), l("filled")],
+          [bn, n(`${DOT}color`), l("red")],
+        ])
+      ),
+      find_blank_nodes(store)
+    )
+  );
 }
 
-store2.into(
-  tx.mapcat(
-    bn => [
-      [bn, n(`${DOT}shape`), l("square")],
-      [bn, n(`${DOT}label`), l("")],
-      [bn, n(`${DOT}width`), l(0.1)],
-      [bn, n(`${DOT}style`), l("filled")],
-    ],
-    find_blank_nodes(store)
-  )
-);
-
-const dot_edges = find_dot_edges(store2, n("Carol"), n("Alice"));
-if (dot_edges) {
-  const [dot_edge] = dot_edges;
-  console.log(`dot_edge`, dot_edge);
-  if (dot_edge) store2.into([[dot_edge, n(`${DOT}color`), l("green")]]);
+function make_dot_store_from(store) {
+  const dot_store = new RDFTripleStore();
+  dot_store.into(triples_to_dot_description(store));
+  mark_bnodes(dot_store);
+  return dot_store;
 }
 
-const { dot_interpret_rdf_store } = require("./lib/dot-interpret-rdf-store");
-//const dot_statements = [...dot_interpret_rdf_store(store)];
-const dot_statements = [...dot_interpret_rdf_store(store2)];
+function get_source_triples() {
+  const { some_object_graph } = require("./lib/test-object-graph");
+  const { some_ast } = require("./lib/some-ast");
+  const { evaluate_cases } = require("./lib/evaluate-cases");
+  const {
+    simple_records,
+    symmetric_property_with_bnodes,
+  } = require("./lib/rdf-js-examples");
+  // const trips = [...rdf_js_traversal(evaluate_cases)];
+  trips = [...symmetric_property_with_bnodes, [n("a"), n("b"), n("c")]];
+  // const thing = trips;
 
-const { rdfjs_store_to_dot_statements } = require("./lib/rdf-js-to-dot");
+  // store.add([n("dolphins"), n(`${DOT}shape`), l("circle")]);
+  // store.add([b("b2536"), n(`${DOT}color`), l("red")]);
+}
+
+function get_entail_case(n = 0) {
+  const entail_cases = require("./lib/simple-entailment-test-cases");
+  return Object.values(entail_cases)[n];
+}
+
+const store_from = triples => {
+  const store = new RDFTripleStore();
+  store.into(triples);
+  return store;
+};
+
+const case_number = 4;
+const source_store_1 = store_from(get_entail_case(case_number).a);
+const dot_store_1 = make_dot_store_from(source_store_1);
+const dot_statements_1 = [...dot_interpret_rdf_store(dot_store_1)];
+
+const source_store_2 = store_from(get_entail_case(case_number).b);
+const dot_store_2 = make_dot_store_from(source_store_2);
+const dot_statements_2 = [...dot_interpret_rdf_store(dot_store_2)];
+
+// mark_node(dot_store_1, n("Bob"));
+// mark_edge(dot_store_1, n("Carol"), n("Alice"));
+
 // const dot_statements = [...rdfjs_store_to_dot_statements(blah)];
 
-// const tx = require("@thi.ng/transducers");
 // exports.display = { things: [...tx.flatten(thing)] };
 
-exports.display = { dot_statements };
+// exports.display = { dot_statements };
+exports.display = {
+  dot_statements: [...dot_statements_1, ...dot_statements_2],
+};
+//exports.display = { dot_graph: { strict: false, statements: dot_statements } };
