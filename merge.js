25816b2bb3684478b74992420595f4e0fa3f1942
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-10 16:41:20 -0500

    apply bnode mapping to each bnode-subgraph
    - extract mapping notation functions to lib
    - extract pre-processing phase (source only) to separate operation
    - WIP on displaying mappings in bnode islands

--- merge.js
+++ merge.js
@@ -1,49 +1,33 @@
 const tx = require("@thi.ng/transducers");
-const { RDFTripleStore, factory } = require("@def.codes/rstream-query-rdf");
-const { map_object } = require("@def.codes/helpers");
+const { RDFTripleStore } = require("@def.codes/rstream-query-rdf");
+const { merge_preprocess_source } = require("./lib/merge-graphs");
 const cases = require("./lib/simple-merge-cases");
-const { bnodes_in, is_blank_node } = require("./lib/graph-ops");
+const { simple_entailment_mapping } = require("./lib/graph-ops");
 const { dot_notate } = require("./lib/dot-notate");
 const { clusters_from } = require("./lib/clustering");
 const { color_connected_components } = require("./lib/color-connected");
-const {
-  triple_store_graph,
-  connected_component_nodes,
-} = require("@def.codes/graphs");
-
-const triples_with_blank_nodes = triples =>
-  tx.filter(([s, , o]) => is_blank_node(s) || is_blank_node(o), triples);
+const { notate_mapping } = require("./lib/notate-mapping");
 
 // merge the triples from graph b into graph a using simple entailment
 const merge_graphs_simple = (a, b) => {
   // part 1: analyze incoming graph
-  //   1a: collect subgraph comprising only the bnodes
-  const triples_with_bnodes = [...triples_with_blank_nodes(b.triples)];
+  const part1 = merge_preprocess_source(b);
+  const { triples_with_bnodes, bnode_components, bnode_islands } = part1;
 
-  //   1b: separate bnodes into connected components
-  const bnodes_store = new RDFTripleStore(triples_with_bnodes);
-  const graph = triple_store_graph(bnodes_store);
-  const bnode_components = connected_component_nodes(graph);
-
-  //   1c: restore non-bnode triples to each bnode subgraph
-  // const restored_subgraphs = [tx.flatten(bnode_components.map(ids => ))];
-  const bnode_islands = bnode_components.map(ids => {
-    return b.triples.filter(([s, , o]) => ids.has(s) || ids.has(o));
-  });
-  // each subgraph must remain distinct
+  const mappings = bnode_islands.map(island =>
+    simple_entailment_mapping(a, new RDFTripleStore(island))
+  );
 
   // part 2: determine existing entailment
   //   2a: for each resulting subgraph, attempt node mapping into target
   //   2b: if match, discard (asserting that substituted facts exist)
   //   2c: if no match, map new minted bnodes to existing ones
+
   // part 3: perform merge
   //   3a: (to view incoming) remove facts already in target
   //   3b: insert resulting facts into target
-  return {
-    triples_with_bnodes,
-    bnode_components,
-    bnode_islands,
-  };
+
+  return { ...part1, mappings };
 };
 
 function do_merge({ source, target, merged }) {
@@ -56,18 +40,25 @@ const [case_name, merge_case] = Object.entries(cases)[
   Object.entries(cases).length - 1
 ];
 
-const { triples_with_bnodes, bnode_components, bnode_islands } = do_merge(
-  merge_case
-);
+const {
+  triples_with_bnodes,
+  bnode_components,
+  bnode_islands,
+  mappings,
+} = do_merge(merge_case);
 
 const bnodes_store = new RDFTripleStore(triples_with_bnodes);
 const components = dot_notate(triples_with_bnodes, "gray");
 const color_notes = [...color_connected_components(bnode_components)];
 
+const target = dot_notate(merge_case.target, "blue").dot_statements;
+
 const dot_statements = clusters_from({
   components: components.dot_statements,
   bnode_components: [...color_notes, ...components.dot_statements],
   bnode_islands: [
+    ...target,
+    ...tx.flatten(mappings.map(m => notate_mapping(m))),
     ...clusters_from(
       Object.fromEntries(
         Object.entries(bnode_islands).map(([key, trips]) => [
@@ -78,7 +69,7 @@ const dot_statements = clusters_from({
     ),
   ],
   source: dot_notate(merge_case.source, "red").dot_statements,
-  target: dot_notate(merge_case.target, "blue").dot_statements,
+  target,
   merged: dot_notate(merge_case.merged, "purple").dot_statements,
 }).map(_ => ({ ..._, attributes: { label: _.id.slice("cluster ".length) } }));
 
@@ -88,7 +79,7 @@ exports.display = {
     directed: true,
     attributes: {
       label: case_name,
-      // rankdir: "LR",
+      rankdir: "LR",
       // layout: "circo",
     },
     statements: dot_statements,
--- triples-to-dot-2.js
+++ triples-to-dot-2.js
@@ -1,18 +1,15 @@
 // const { inspect } = require("util");
 // const tx = require("@thi.ng/transducers");
 const { equiv } = require("@thi.ng/equiv");
-const { DOT, TYPES } = require("@def.codes/graphviz-format");
-const { NODE, EDGE } = TYPES;
-const { RDFTripleStore, factory } = require("@def.codes/rstream-query-rdf");
-const { dot_interpret_rdf_store } = require("./lib/dot-interpret-rdf-store");
+const { DOT } = require("@def.codes/graphviz-format");
+const { factory } = require("@def.codes/rstream-query-rdf");
 const { prefix_statement_keys, clusters_from } = require("./lib/clustering");
 const entail_cases = require("./lib/simple-entailment-test-cases");
 const { simple_entailment_mapping } = require("./lib/graph-ops");
 const { dot_notate } = require("./lib/dot-notate");
+const { notate_mapping } = require("./lib/notate-mapping");
 
 const { namedNode: n, blankNode: b, literal: l, variable: v } = factory;
-
-const TYPE = n("rdf:type");
 
 /* other sources
 function get_source_triples() {
@@ -32,16 +29,6 @@ const do_entail_case = entail_case => ({
   b: dot_notate(entail_case.b, "red"),
 });
 
-const make_dot_edge = (from, to, attrs = {}) => {
-  const edge = b();
-  return [
-    [edge, TYPE, n(EDGE)],
-    [edge, n(`${DOT}from`), from],
-    [edge, n(`${DOT}to`), to],
-    ...Object.entries(attrs).map(([k, v]) => [edge, n(`${DOT}${k}`), l(v)]),
-  ];
-};
-
 function case_statements(entail_case) {
   const { a, b } = do_entail_case(entail_case);
 
@@ -58,39 +45,20 @@ function case_statements(entail_case) {
   };
 }
 
-// mark algorithm state
-function mark_algorithm(A, B) {
-  const store = new RDFTripleStore();
-
-  const mapping = simple_entailment_mapping(A.source, B.source);
-  for (const [from, target] of mapping) {
-    // console.log(`from, target`, from, target);
-    store.into([
-      // what we really mean is the node representing Socrates
-      // ...make_dot_edge(n("Socrates"), n("Greek"), {
-      ...make_dot_edge(from, target, {
-        constraint: false,
-        color: "#FF00FF88",
-        penwidth: 5,
-      }),
-    ]);
-  }
-
-  return dot_interpret_rdf_store(store);
-}
-
 // this no longer really plays well with clusters mode
 function do_case(number = 0, mode = 0) {
   const [case_name, entail_case] = Object.entries(entail_cases)[number];
   const { a: A, b: B, clusters, merged } = case_statements(entail_case);
   const base_dot_statements = [merged, clusters][mode];
+  const mapping = simple_entailment_mapping(A.source, B.source);
+
   return {
     type: "subgraph",
     id: `cluster case ${number}`,
     attributes: { label: case_name },
     statements: prefix_statement_keys(`c${number} `)([
       ...base_dot_statements,
-      ...mark_algorithm(A, B),
+      ...notate_mapping(mapping),
     ]),
   };
 }
