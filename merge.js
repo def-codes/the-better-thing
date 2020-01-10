b9e906637dd707bce133f0d234549ffd5ef7dc1a
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-10 01:12:49 -0500

    merge strategy, test cases for bnodes forming disconnected components; test case cleanup

--- merge.js
+++ merge.js
@@ -4,11 +4,23 @@ const { dot_notate } = require("./lib/dot-notate");
 const { clusters_from } = require("./lib/clustering");
 
 const [case_name, merge_case] = Object.entries(cases)[
-  Object.entries(cases).length - 4
+  Object.entries(cases).length - 2
 ];
 
 // merge the triples from graph b into graph a using simple entailment
-const merge_graphs_simple = (a, b) => {};
+const merge_graphs_simple = (a, b) => {
+  // part 1: analyze incoming graph
+  //   1a: collect subgraph comprising only the bnodes
+  //   1b: separate bnodes into connected components
+  //   1c: restore non-bnode triples to each bnode subgraph
+  // part 2: determine existing entailment
+  //   2a: for each resulting subgraph, attempt node mapping into target
+  //   2b: if match, discard (asserting that substituted facts exist)
+  //   2c: if no match, map new minted bnodes to existing ones
+  // part 3: perform merge
+  //   3a: (to view incoming) remove facts already in target
+  //   3b: insert resulting facts into target
+};
 
 const dot_statements = clusters_from({
   source: dot_notate(merge_case.source, "red").dot_statements,
@@ -18,5 +30,9 @@ const dot_statements = clusters_from({
 
 exports.display = {
   // thing: entail_case,
-  dot_graph: { attributes: { label: case_name }, statements: dot_statements },
+  dot_graph: {
+    directed: true,
+    attributes: { label: case_name },
+    statements: dot_statements,
+  },
 };
