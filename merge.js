b60f30e7fe9b5ef31efb16769975e371a92dcb0f
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-14 23:33:21 -0500

    extract simple merge function to lib

--- merge.js
+++ merge.js
@@ -1,9 +1,8 @@
 const { inspect } = require("util");
 const tx = require("@thi.ng/transducers");
 const { RDFTripleStore, factory } = require("@def.codes/rstream-query-rdf");
-const { merge_preprocess_source } = require("./lib/merge-graphs");
 const cases = require("./lib/example-graph-pairs");
-const { simple_entailment_mapping, is_blank_node } = require("./lib/graph-ops");
+const { simple_merge_triples } = require("./lib/simple-merge");
 const { dot_notate } = require("./lib/dot-notate");
 const { clusters_from } = require("./lib/clustering");
 const { color_connected_components } = require("./lib/color-connected");
@@ -11,55 +10,8 @@ const { notate_mapping } = require("./lib/notate-mapping");
 
 const { blankNode: b } = factory;
 
-// merge the triples from graph b into graph a using simple entailment
-const merge_graphs_simple = (a, b) => {
-  // part 1: analyze incoming graph
-  const part1 = merge_preprocess_source(b);
-  const { triples_with_bnodes, bnode_components, bnode_islands } = part1;
-
-  // part 2: separate already-entailed subgraphs
-
-  //   2a: for each resulting subgraph, attempt node mapping into target
-  const mappings_base = bnode_islands.map(island => ({
-    island,
-    mapping: simple_entailment_mapping(a, new RDFTripleStore(island)),
-  }));
-
-  const extend = ({ island, mapping }) => {
-    if (mapping.size === 0) {
-      //   2c: if no match, map new minted bnodes to existing ones
-      const map = new Map();
-      const sub = term => {
-        if (!is_blank_node(term)) return term;
-        if (!map.has(term)) map.set(term, factory.blankNode());
-        return map.get(term);
-      };
-      return { minted: island.map(([s, p, o]) => [sub(s), p, sub(o)]) };
-    }
-    // 2b: if match, discard (asserting that substituted facts exist)
-    const sub = term => mapping.get(term) || term;
-    return {
-      entailed: island.map(triple => {
-        const mapped = triple.map(sub);
-        return { triple, mapped, pass: a.has(mapped) };
-      }),
-    };
-  };
-
-  const mappings = mappings_base.map(item => ({ ...item, ...extend(item) }));
-  const incoming = [
-    ...tx.mapcat(x => x, tx.keep(tx.map(_ => _.minted, mappings))),
-    ...b.triples.filter(triple => !triple.some(is_blank_node)),
-  ];
-
-  return { ...part1, mappings, incoming };
-};
-
-function do_merge({ source, target, merged }) {
-  const source_store = new RDFTripleStore(source);
-  const target_store = new RDFTripleStore(target);
-  return merge_graphs_simple(target_store, source_store);
-}
+const do_merge_case = ({ source, target }) =>
+  simple_merge_triples(target, source);
 
 const [case_name, merge_case] = Object.entries(cases)[41];
 // Object.entries(cases).length - 4
@@ -70,7 +22,7 @@ const {
   bnode_islands,
   mappings,
   incoming,
-} = do_merge(merge_case);
+} = do_merge_case(merge_case);
 console.log(`incoming`, incoming);
 
 for (const { entailed } of mappings) {
