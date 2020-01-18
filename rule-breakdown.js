e9d74229fc6905b7b02ab11e8da39aaf3cddcc8d
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-18 09:36:06 -0500

    ibid

--- rule-breakdown.js
+++ rule-breakdown.js
@@ -10,7 +10,13 @@ const { sync_query, RDFTripleStore } = require("@def.codes/rstream-query-rdf");
 
 const examples = require("./lib/example-graph-pairs");
 /// const { target } = examples["The author of Symposium is a student of Socrates"];
-const target = q("Bob loves Alice", "Alice loves Carol");
+const target = q(
+  "Bob loves Alice",
+  "Alice loves Carol",
+  "_:xyz age 31",
+  `_:xyz commonName "Fela"`,
+  "?who loves me"
+);
 
 const antecedent = q("?s ?p ?o");
 const source_store = new RDFTripleStore(target);
@@ -20,7 +26,7 @@ const dot_statements = clusters_from({
   source: dot_notate(source_store.triples).dot_statements,
   source_triples: things_to_dot_statements(source_store.triples).dot_statements,
   antecedent: dot_notate(antecedent).dot_statements,
-  result: things_to_dot_statements(result).dot_statements,
+  result: things_to_dot_statements(result || []).dot_statements,
 }).map(_ => ({ ..._, attributes: { label: _.id.slice("cluster ".length) } }));
 
 exports.display = { dot_statements };
