ed712c5f24dea50785b869a6f8bec9833e754839
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-20 10:03:23 -0500

    support multiple rules (but still wrong)

--- rules2.js
+++ rules2.js
@@ -1,7 +1,7 @@
 // see the stages in the application of rules
 const tx = require("@thi.ng/transducers");
 const show = require("./lib/thing-to-dot-statements");
-const { apply_rule } = require("./lib/rules2");
+const { apply_rules } = require("./lib/rules2");
 const { dot_notate } = require("./lib/dot-notate");
 const { clusters_from } = require("./lib/clustering");
 const { q } = require("@def.codes/meld-core");
@@ -17,42 +17,37 @@ const prep = (...cs) =>
 
 const DOT_SUBJECT_RULE = {
   name: "DotSubjectNodeRule",
-  when: q("?s ?p ?o"),
-  then: () => ({
-    assert: prep(
-      "_:sub a dot:Node",
-      "_:sub def:represents ?s"
-      // "_:obj a dot:Node",
-      // "_:obj def:represents ?o"
-      // `?sub dot:color "green"` // TEMP debug
-    ),
-  }),
+  antecedent: q("?s ?p ?o"),
+  consequent: prep("_:sub a dot:Node", "_:sub def:represents ?s"),
+};
+
+const DOT_OBJECT_RULE = {
+  name: "DotObjectNodeRule",
+  antecedent: q("?s ?p ?o"),
+  consequent: prep("_:obj a dot:Node", "_:obj def:represents ?o"),
 };
 
 const LOVE_TRIANGLE_RULE = {
   name: "LoveTriangleRule",
-  when: q("?x loves ?y", "?y loves ?z", "?z loves ?x"),
-  then: () => ({
-    assert: prep(
-      // better than `hates`
-      "?x jealousOf ?z",
-      "?y jealousOf ?x",
-      "?z jealousOf ?y",
-      "_:someone pities ?x"
-    ),
-  }),
+  antecedent: q("?x loves ?y", "?y loves ?z", "?z loves ?x"),
+  consequent: prep(
+    // better than `hates`
+    "?x jealousOf ?z",
+    "?y jealousOf ?x",
+    "?z jealousOf ?y",
+    "_:someone pities ?x"
+  ),
 };
 
 const rule = [DOT_SUBJECT_RULE, LOVE_TRIANGLE_RULE][0];
-const antecedent = rule.when;
-const consequent = rule.then().assert; // note it is nullary
+const rules = [DOT_SUBJECT_RULE, DOT_OBJECT_RULE];
 
 const source_store = new RDFTripleStore(source);
 // For derived graph
 const target_store = new RDFTripleStore([], source_store.blank_node_space_id);
 // For new graph
 // const target_store = new RDFTripleStore();
-apply_rule({ antecedent, consequent }, source_store, target_store);
+apply_rules(rules, source_store, target_store);
 console.log(`target_store.triples`, target_store.triples);
 
 const { curied_triples, curied_term } = require("./curie");
@@ -60,8 +55,8 @@ const { curied_triples, curied_term } = require("./curie");
 const dot_statements = clusters_from({
   source: dot_notate(source_store.triples).dot_statements,
   source_triples: show.things(source_store.triples).dot_statements,
-  antecedent: dot_notate(antecedent).dot_statements,
-  consequent: dot_notate(consequent).dot_statements,
+  // antecedent: dot_notate(antecedent).dot_statements,
+  // consequent: dot_notate(consequent).dot_statements,
   target: dot_notate(target_store.triples).dot_statements,
   target_triples: show.things(target_store.triples).dot_statements,
 }).map(_ => ({ ..._, attributes: { label: _.id.slice("cluster ".length) } }));
