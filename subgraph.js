d30c62367a058effe8865f63004fb01c103662f4
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-22 15:06:06 -0500

    display subgraph view output as coloring in merged view

--- subgraph.js
+++ subgraph.js
@@ -1,5 +1,7 @@
 // subgraph views
 const { from_facts, SubgraphView } = require("@def.codes/graphs");
+const show = require("./lib/thing-to-dot-statements");
+const { clusters_from } = require("./lib/clustering");
 
 const FACTS = [
   { subject: "a", value: "a" },
@@ -56,4 +58,36 @@ const test_case_number = 3;
 const test_case = TEST_CASES[test_case_number];
 const { graph, subgraph } = do_test_case(test_case);
 
-exports.display = { graph: subgraph };
+const graph_statements = show.graph(graph).dot_statements;
+const subgraph_statements = show.graph(subgraph).dot_statements;
+
+const dot_statements = clusters_from({
+  graph: graph_statements,
+  subgraph: subgraph_statements,
+  merged: [
+    ...graph_statements.filter(
+      x =>
+        !subgraph_statements.find(
+          ({ type, id, from, to }) =>
+            (x.type === "node" && type === "node" && x.id === id) ||
+            (x.type === "edge" &&
+              type === "edge" &&
+              x.from === from &&
+              x.to === to)
+        )
+    ),
+    ...subgraph_statements.map(({ attributes, ...rest }) => {
+      return {
+        ...rest,
+        attributes: { ...(attributes || {}), penwidth: 5, color: "#00009977" },
+      };
+    }),
+  ],
+}).map(_ => ({ ..._, attributes: { label: _.id.slice("cluster ".length) } }));
+
+exports.display = {
+  dot_graph: {
+    directed: true,
+    statements: dot_statements,
+  },
+};
