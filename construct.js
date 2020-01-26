db348b34b6c90a7706edb347b3660827aae14d27
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-01-25 22:39:38 -0500

    create separate sandbox for construction pipeline (like dot but without)
    create sample graph based on some info from DADS

--- construct.js
+++ construct.js
@@ -1,13 +1,12 @@
-// see the stages in the application of rules
-// this is just a CONSTRUCT query
-const tx = require("@thi.ng/transducers");
 const show = require("./lib/show");
+const { q } = require("@def.codes/meld-core");
 const { construct } = require("./lib/construct");
 const { clusters_from } = require("./lib/clustering");
-const { q } = require("@def.codes/meld-core");
 const { RDFTripleStore } = require("@def.codes/rstream-query-rdf");
-const { DOT } = require("@def.codes/graphviz-format");
+const { LOVE_TRIANGLE } = require("./graphs/simple");
+const { DADS_UPPER_ONTOLOGY } = require("./graphs/dads");
 
+/*
 const examples = require("./lib/example-graph-pairs");
 /// const { target } = examples["The author of Symposium is a student of Socrates"];
 const source = q(
@@ -16,127 +15,73 @@ const source = q(
   "Carol loves Alice",
   "Dave loves Carol"
 );
+*/
 
-const prep = (...cs) =>
-  q(...cs.map(_ => _.replace(/dot:/g, DOT).replace(/ a /g, " rdf:type ")));
-
-const COPY_RULE = {
-  name: "CopyRule",
-  where: q("?s ?p ?o"),
-  construct: q("?s ?p ?o"),
-};
-
-const DOT_NODE_RULE = {
-  name: "DotNodeRule",
-  where: q("?s ?p ?o"),
-  construct: prep(
-    "_:sub a dot:Node",
-    "_:sub def:represents ?s",
-    "_:obj a dot:Node",
-    "_:obj def:represents ?o"
-  ),
-};
-
-const DOT_SUBJECT_RULE = {
-  name: "DotSubjectNodeRule",
-  where: q("?s ?p ?o"),
-  construct: prep("_:sub a dot:Node", "_:sub def:represents ?s"),
-};
-
-const DOT_OBJECT_RULE = {
-  name: "DotObjectNodeRule",
-  where: q("?s ?p ?o"),
-  construct: prep("_:obj a dot:Node", "_:obj def:represents ?o"),
-};
-
-const DOT_EDGE_RULE = {
-  name: "DotEdgeRule",
-  where: prep(
-    "?subject ?predicate ?object",
-    "?from a dot:Node",
-    "?from def:represents ?subject",
-    "?to a dot:Node",
-    "?to def:represents ?object"
-  ),
-  construct: prep(
-    "_:edge a dot:Edge",
-    "_:edge dot:from ?from",
-    "_:edge dot:to ?to",
-    "_:edge def:represents _:trip",
-    "_:trip rdf:subject ?subject",
-    "_:trip rdf:predicate ?predicate",
-    "_:trip rdf:object ?object"
-  ),
-};
-
-const DOT_LABEL_RULE_0 = {
-  name: "DotLabelRule",
-  where: prep("?n a dot:Node"),
-  construct: prep(`?n dot:color "red"`, `?n dot:style "filled"`),
-};
-
-const DOT_LABEL_RULE = {
-  name: "DotLabelRule",
-  where: prep("?n a dot:Node", "?n def:represents ?s"),
-  // this kinda works but not for good reason
-  construct: prep(`?n dot:label ?s`),
-};
-
-const LOVE_TRIANGLE_RULE = {
-  name: "LoveTriangleRule",
-  where: q("?x loves ?y", "?y loves ?z", "?z loves ?x"),
-  construct: prep(
-    // better than `hates`
-    "?x jealousOf ?z",
-    "?y jealousOf ?x",
-    "?z jealousOf ?y",
-    "_:someone pities ?x"
-  ),
-};
-
-const rule = [DOT_SUBJECT_RULE, LOVE_TRIANGLE_RULE][0];
-// COPY_RULE seems not to be working
-const rules = [
-  // COPY_RULE,
-  DOT_NODE_RULE,
-  // DOT_SUBJECT_RULE,
-  // DOT_OBJECT_RULE,
-  // DOT_EDGE_RULE,
-  // DOT_LABEL_RULE,
+const TEST_CASES = [
+  {
+    label: `copy input`,
+    triples: LOVE_TRIANGLE,
+    rules: [{ construct: q(`?s ?p ?o`) }],
+  },
+  {
+    label: `reverse love`,
+    triples: LOVE_TRIANGLE,
+    rules: [{ construct: q(`?s loves ?o`), where: q(`?o loves ?s`) }],
+  },
+  {
+    label: `make love symmetrical`,
+    triples: LOVE_TRIANGLE,
+    rules: [
+      { construct: q(`?s ?p ?o`) },
+      { construct: q(`?s loves ?o`), where: q(`?o loves ?s`) },
+    ],
+  },
+  {
+    label: `jealousy & pity`,
+    triples: LOVE_TRIANGLE,
+    rules: [
+      { construct: q(`?s ?p ?o`) },
+      {
+        construct: q(
+          "?x jealousOf ?z",
+          "?y jealousOf ?x",
+          "?z jealousOf ?y",
+          "_:someone pities ?x"
+        ),
+        where: q("?x loves ?y", "?y loves ?z", "?z loves ?x"),
+      },
+    ],
+  },
+  {
+    label: `DADS upper ontology`,
+    triples: DADS_UPPER_ONTOLOGY,
+    // this is unreadable, presumably because there are long labels
+    rules: [{ construct: q(`?s ?p ?o`) }],
+  },
 ];
 
-const source_store = new RDFTripleStore(source);
-// For derived graph
-const target_store = new RDFTripleStore(
-  source_store.triples,
-  source_store.blank_node_space_id
-);
-// For new graph
-// const target_store = new RDFTripleStore();
-construct(rules, source_store, target_store);
-// console.log(`target_store.triples`, target_store.triples);
+const do_case = test_case => {
+  const { label, triples, rules } = test_case;
+  const source_store = new RDFTripleStore(triples);
 
-const second_target_store = new RDFTripleStore(
-  target_store.triples,
-  target_store.blank_node_space_id
-);
-construct([DOT_LABEL_RULE, DOT_EDGE_RULE], target_store, second_target_store);
+  // Use same blank node space id to create “derived” graph
+  const target_store = new RDFTripleStore([], source_store.blank_node_space_id);
+  construct(rules, source_store, target_store);
 
-const { curied_triples, curied_term } = require("./lib/curie");
+  const dot_statements = clusters_from({
+    source: show.store(source_store),
+    rules: show.things(rules),
+    result: show.store(target_store),
+  });
+  return { test_case_label: label, dot_statements };
+};
 
-const { dot_interpret_rdf_store } = require("./lib/dot-interpret-rdf-store");
-const interpreted = [...dot_interpret_rdf_store(second_target_store)];
+const { test_case_label, dot_statements } = do_case(TEST_CASES[4]);
 
-const dot_statements = clusters_from({
-  source: show.store(source_store),
-  source_triples: show.things(source_store.triples),
-  // where: show.triples(where),
-  // construct: show.triples(construct),
-  target: show.store(target_store),
-  target_triples: show.things(target_store.triples),
-  second_target: show.store(second_target_store),
-  second_target_triples: show.things(second_target_store.triples),
-  interpreted,
-});
-
-exports.display = { dot_statements };
+exports.display = {
+  dot_graph: {
+    directed: true,
+    attributes: { label: test_case_label, labelloc: "t" },
+    statements: dot_statements,
+  },
+};
---
+++ graphs/simple.js
@@ -0,0 +1,7 @@
+const { q } = require("@def.codes/meld-core");
+
+exports.LOVE_TRIANGLE = q(
+  "Bob loves Alice",
+  "Alice loves Carol",
+  "Carol loves Bob"
+);
