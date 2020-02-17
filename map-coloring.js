e7c7880f5e992e728d22e2cc929fb7b76bb1eb38
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-02-16 20:11:45 -0500

    towards view of multiple search states (assignments); sketching on CSP algo

--- map-coloring.js
+++ map-coloring.js
@@ -63,8 +63,18 @@ const main = () => {
     .map(([s]) => [s, TYPE, VARIABLE]);
   problem_store.into(variables);
 
+  // Is there a definition of the domain for each variable that is distinct from
+  // its domain at any given point in the search? i.e. when you proceed by
+  // domain reduction, is there not some definition of the domain that precedes
+  // the work and stands as part of the definition?
+
   // The variables are part of a problem.  Associate variables with problem.
   problem_store.into(variables.map(([s]) => [problem, HAS_VARIABLE, s]));
+
+  // DOMAINS
+  //
+  // All states have the same domain
+  // Do we need to repeat the whole set for each one?
 
   // ==== MAP VIEW
 
@@ -78,24 +88,86 @@ const main = () => {
     usa_states_triples.filter(_ => _[1] !== LABEL && _[1] !== TYPE)
   );
 
+  // ==== SEARCH SPACE
+  const triples = q("a p b", "b p c", "c p a", "c p d");
+  const triples_statements = show.triples_old(triples).map(unlabel_edge);
+  const assignments = [
+    {},
+    { a: "red" },
+    { a: "blue" },
+    { a: "red", b: "blue" },
+    { a: "red", b: "blue", c: "darkgreen" },
+    { b: "blue" },
+  ];
+  const connect = [
+    [0, 1],
+    [0, 2],
+    [1, 3],
+    [3, 4],
+  ];
+  const color_with = (assignment, statements = triples_statements) =>
+    Object.entries(assignment).map(([id, color]) => ({
+      type: "node",
+      id: id,
+      attributes: { style: "filled", color, fontcolor: "white" },
+    }));
+  const search_space = Object.fromEntries(
+    assignments.map((assignment, index) => [
+      index,
+      Object.assign(
+        [
+          { type: "node", id: "_", attributes: { style: "invis" } },
+          ...triples_statements,
+          ...color_with(assignment),
+        ],
+        {
+          attributes: {
+            label: Object.entries(assignment)
+              .map(([k, v]) => `${k}:${v}`)
+              .join(","),
+          },
+        }
+      ),
+    ])
+  );
+
   // ==== OVERVIEW ==== //
 
   // newer rules-based display is currently way too slow for this purpose
-  const statements = clusters_from({
-    problem: show.store_old(problem_store),
-    map_view: show
-      // edge_label override is for new show.store method, which is currently not used
-      .store_old(map_view, { edge_label: [] }) //  edges are all “us:borders”
-      .map(unlabel_edge),
-  });
+  const statements = [
+    // wonky but the only way I could get arrows between the clusters
+    ...connect.map(([from, to]) => ({
+      type: "edge",
+      from: `search_space/${from}/_`,
+      to: `search_space/${to}/_`,
+      attributes: {
+        dir: "forward",
+        // Only works in `dot` engine, but dot is otherwise bad
+        ltail: `cluster search_space/${from}`,
+        lhead: `cluster search_space/${to}`,
+      },
+    })),
+    ...clusters_from({
+      // problem: show.store_old(problem_store),
+      search_space,
+      // map_view: show
+      //   // edge_label override is for new show.store method, which is currently not used
+      //   .store_old(map_view, { edge_label: [] }) //  edges are all “us:borders”
+      //   .map(unlabel_edge),
+    }),
+  ];
+  console.log(`statements`, require("util").inspect(statements, { depth: 6 }));
 
   const dot_graph = {
     type: "graph",
     strict: true,
     node_attributes: { shape: "circle" },
+    edge_attributes: { len: 0.05 },
+
     // fdp preserves cluster boundaries & their labels
-    // neato produces something almost completely flat & resembling US
-    attributes: { layout: "neato" },
+    // neato is really good for map; with short edge length, fdp is almost as good
+    // only dot handles inter-cluster links reasonably
+    attributes: { compound: true, layout: "fdp" },
     statements,
   };
 
