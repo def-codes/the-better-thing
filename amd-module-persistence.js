819ce1a9c4d52e61274111a1351edec39c7e1dca
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-03-26 23:14:01 -0400

    fix(portability): bug in literate AMD module persistence

--- amd-module-persistence.js
+++ amd-module-persistence.js
@@ -12,12 +12,15 @@
     const create = tag => document.createElement(tag);
     const append = (parent, child) => parent.appendChild(child);
 
+    // Prevent an infinite loop from adding this repeatedly.
+    if (document.querySelector(`[data-amd-module="${definition.id}"]`)) return;
+
     const cont = document.getElementById("amd-modules");
     const article = append(cont, create("article"));
     const details = append(article, create("details"));
     const summary = append(details, create("summary"));
     const heading = append(summary, create("h3"));
-    const script = append(details, create("pre"));
+    const script = append(details, create("script"));
 
     heading.innerHTML = definition.id;
     script.setAttribute("data-amd-module", definition.id);
