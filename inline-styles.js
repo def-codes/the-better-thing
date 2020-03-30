573f3cb5d5276ce3aa3742f0b211117da01bc440
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-03-30 00:12:20 -0400

    inlined styles are in the way

--- inline-styles.js
+++ inline-styles.js
@@ -1,12 +1,26 @@
 require([], () => {
   const links = [...document.querySelectorAll(`link[rel="stylesheet"]`)];
+
+  // By default, this inlines stylesheets in place of the import links, which is
+  // preserves lexical order.  But when the document head and style blocks are
+  // made visible, the inlined style definitions are quite in the way.  If an
+  // `#inlined-styles` container is present, it will be used instead.  This may
+  // break the original lexical ordering, but lets you control how the content
+  // appears.
+  const container = document.querySelector("#inlined-styles");
   for (const link of links) {
     const style = document.createElement("style");
     const css = [...link.sheet.cssRules].map(_ => _.cssText).join("\n");
     const text = document.createTextNode(css);
     style.appendChild(text);
-    // link.parentNode.replaceChild(style, link);
-    document.head.appendChild(style);
-    link.parentNode.removeChild(link);
+    if (container) {
+      container.appendChild(style);
+      link.parentNode.removeChild(link);
+    } else {
+      // This was commented and replaced with below.  Seems to be working now.
+      link.parentNode.replaceChild(style, link);
+      // document.head.appendChild(style);
+      // link.parentNode.removeChild(link);
+    }
   }
 });
