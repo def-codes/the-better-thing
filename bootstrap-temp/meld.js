5aaa67a6ba5cf620d15acf902b68a8f91695432c
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-04-26 21:11:14 -0400

    cooking I

--- bootstrap-temp/meld.js
+++ bootstrap-temp/meld.js
@@ -1,6 +1,29 @@
 // TODO: This should not have path, should be relative to this
 require(["./bootstrap-temp/meld-system.js"], ({ system }) => {
-  console.log(`system`, system);
+  const { dataset } = system;
+  const { defaultGraph, factory } = dataset;
+
+  const TYPE = factory.namedNode("rdf:type");
+  const GRAPH = factory.namedNode("rdfg:Graph");
+
+  // or make one, etc
+  const root = document.querySelector("#named-graphs");
+
+  defaultGraph
+    .addQueryFromSpec({ q: [{ where: [["?s", TYPE, GRAPH]] }] })
+    .subscribe({
+      next(results) {
+        // Assume it's not a blank node
+        const iris = Array.from(results, _ => _.s.value);
+        for (const iri of iris) {
+          const selector = `graph=["${iri}"]`;
+        }
+        console.log(`results`, foo);
+      },
+    });
+
+  const some_graph = dataset.create();
+
   // now what
   // give the system a model to interpret
   // where do you get that
