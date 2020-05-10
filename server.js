4bd804a540d94beeccb383ffa7c5e41d5daf3dc0
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-05-10 08:54:56 -0400

    hack: add source mapping url's for @thi.ng libraries

--- server.js
+++ server.js
@@ -3,10 +3,28 @@ const {
   with_static_files,
 } = require("@def.codes/simple-http-server");
 const { shell_open } = require("@def.codes/node-web-presentation");
+const fs = require("fs");
 
 const PORT = 1234;
 
-create_server({ port: PORT, handler: with_static_files() });
+// HACK: @thi.ng packages provide source maps but don't point to them.
+const orig_readFileSync = fs.readFileSync;
+const THING_LIB = /\/node_modules\/@thi.ng\/([^\/]+)\/lib\/index\.umd\.js$/;
+fs.readFileSync = function (file) {
+  const thing_lib = file.match(THING_LIB);
+  if (thing_lib) {
+    const [, lib] = thing_lib;
+    return (
+      orig_readFileSync(file) +
+      "\n//# sourceMappingURL=/node_modules/@thi.ng/" +
+      lib +
+      "/lib/index.umd.js.map"
+    );
+  }
+  return orig_readFileSync(file);
+};
+
+create_server({ port: PORT, handler: with_static_files(), fs });
 
 // Open a browser
 shell_open(`http://localhost:${PORT}`);
