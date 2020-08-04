fbb4f4673a19056428857c25002e6bc88ba5814a
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-08-04 01:23:48 -0400

    multi-spaces

--- modules/2020-08.js
+++ modules/2020-08.js
@@ -13,40 +13,39 @@ define([
     const nodes = rs.subscription({ next: sim.nodes });
     const forces = rs.subscription({ next() {} });
 
-    // Add a style element that we'll maintain the old fashioned way
-    // const style_node = root.appendChild(document.createElement("style"));
-    // dom_process.port();
-
-    const frame = () => {
-      const css = sim
-        .nodes()
-        .map(
-          _ =>
-            `#${_.id}{` +
-            `--x:${Math.round(_.x)};--y:${Math.round(_.y)};` +
-            `--fx:${Math.round(_.fx)};--fy:${Math.round(_.fy)};` +
-            `--vx:${Math.round(_.vx)};--fy:${Math.round(_.vy)}}`
-        )
-        .join("\n");
-      // Update all positions
-      dom_process.define(`${id} styles`, {
-        element: "style",
-        attributes: {},
-        children: [css],
-      });
-
-      // style_node.innerHTML =
-    };
-
     // Default forces (just for testing)
+    sim.force("x-axis", d3.forceX(0));
+    sim.force("y-axis", d3.forceY(0));
     sim.force("center", d3.forceCenter());
     sim.force("charge", d3.forceManyBody());
 
-    const ticker = rs.fromInterval(100).transform(
+    // const ticker = rs.fromInterval(150)
+    rs.fromInterval(1000).subscribe({ next: () => sim.restart() });
+    const ticker = rs.fromRAF().transform(
       tx.sideEffect(() => sim.tick()),
       tx.map(() => sim.nodes())
     );
-    ticker.subscribe({ next: frame });
+    ticker.subscribe({
+      next: css => {
+        // Update all positions
+        dom_process.define(`${id} styles`, {
+          element: "style",
+          attributes: {},
+          children: [
+            sim
+              .nodes()
+              .map(
+                _ =>
+                  `#${_.id}{` +
+                  `--x:${Math.round(_.x)};--y:${Math.round(_.y)};` +
+                  `--fx:${Math.round(_.fx)};--fy:${Math.round(_.fy)};` +
+                  `--vx:${Math.round(_.vx)};--fy:${Math.round(_.vy)}}`
+              )
+              .join("\n"),
+          ],
+        });
+      },
+    });
 
     const streams = { ticker, nodes, forces };
 
@@ -70,6 +69,11 @@ define([
   // create graph (if not dataset) to represent model
   //
   // If something is a space, then create a manager/process object for it
+  const box_id = id => ({
+    id,
+    x: Math.random() * 1000,
+    y: Math.random() * 1000,
+  });
 
   function main() {
     const root = document.getElementById("August-2020-space");
@@ -77,73 +81,47 @@ define([
     const dom_process = dp.make_dom_process();
     dom_process.mounted.next({ id: "root", element: root });
 
-    /*
-    dom_process_1.define("alien", {
-      element: "i",
-      attributes: {},
-      children: [
-        "invaaaasion!",
-        { element: "placeholder", attributes: { id: "spook" }, children: [] },
-      ],
-    });
-    // dom_process_1.define("species", template);
-    dom_process_1.define("root1", {
-      element: "b",
-      attributes: {},
-      children: [
-        "foo",
-        { element: "placeholder", attributes: { id: "alien" }, children: [] },
-        { element: "placeholder", attributes: { id: "species" }, children: [] },
-        "bar",
-      ],
-    });
-	*/
-    const names1 = "Alice Bob Carol".split(" ");
-    const names2 = "Dave Edie Frank".split(" ");
+    const spaces = {
+      space1: { names: "Alice Bob Carol".split(" ") },
+      space2: { names: "Dave Edie Frank".split(" ") },
+      space3: { names: "Joe Al Sue".split(" ") },
+    };
+    const space_entries = Object.entries(spaces);
+
     dom_process.define(
       "root",
       operations_to_template([
         { type: "attribute-contains-word", name: "class", value: "panels" },
-        { type: "contains", id: "space1" },
-        { type: "contains", id: "space2" },
+        ...space_entries.map(([id]) => ({ type: "contains", id })),
       ])
     );
-    dom_process.define(
-      "space1",
-      operations_to_template([
-        { type: "attribute-equals", name: "typeof", value: "Space" },
-        { type: "contains", id: `space1 styles` },
-        ...names1.map(id => ({ type: "contains", id })),
-      ])
-    );
-    dom_process.define(
-      "space2",
-      operations_to_template([
-        { type: "attribute-equals", name: "typeof", value: "Space" },
-        { type: "contains", id: `space2 styles` },
-        ...names2.map(id => ({ type: "contains", id })),
-      ])
-    );
-    for (const name of [...names1, ...names2])
+
+    for (const [space, { names }] of space_entries) {
       dom_process.define(
-        name,
+        space,
         operations_to_template([
-          { type: "uses-element", name: "b" },
-          { type: "attribute-equals", name: "id", value: name },
-          {
-            type: "contains-text",
-            text: `My name is ${name.toUpperCase()} fool!`,
-          },
+          { type: "attribute-equals", name: "typeof", value: "Space" },
+          { type: "contains", id: `${space} styles` },
+          ...names.map(id => ({ type: "contains", id })),
         ])
       );
+      for (const name of names)
+        dom_process.define(
+          name,
+          operations_to_template([
+            { type: "uses-element", name: "b" },
+            { type: "attribute-equals", name: "id", value: name },
+            {
+              type: "contains-text",
+              text: `My name is ${name.toUpperCase()} fool!`,
+            },
+          ])
+        );
 
-    const one = make_space({ dom_process, id: "space1" });
-    const two = make_space({ dom_process, id: "space2" });
-    // const { streams } = make_space({ root: root1, dom_process: dom_process_1 });
-    // const { ticker, nodes, forces } = streams;
-    const box_id = id => ({ id });
-    one.streams.nodes.next(names1.map(box_id));
-    two.streams.nodes.next(names2.map(box_id));
+      const { streams } = make_space({ dom_process, id: space });
+      // const { ticker, nodes, forces } = streams;
+      streams.nodes.next(names.map(box_id));
+    }
   }
 
   main();
