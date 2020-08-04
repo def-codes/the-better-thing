32a560832bb2c1e5837856ce4df7b15f95f51c64
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-08-04 00:53:28 -0400

    very strangely, still not not happening

--- modules/2020-08.js
+++ modules/2020-08.js
@@ -8,17 +8,17 @@ define([
   const { facts_to_operations, operations_to_template } = dom_rules;
 
   const make_space = spec => {
-    const { root, dom_process } = spec;
+    const { id, dom_process } = spec;
     const sim = d3.forceSimulation().stop();
     const nodes = rs.subscription({ next: sim.nodes });
     const forces = rs.subscription({ next() {} });
 
     // Add a style element that we'll maintain the old fashioned way
-    const style_node = root.appendChild(document.createElement("style"));
+    // const style_node = root.appendChild(document.createElement("style"));
+    // dom_process.port();
 
     const frame = () => {
-      // Update all positions
-      style_node.innerHTML = sim
+      const css = sim
         .nodes()
         .map(
           _ =>
@@ -28,6 +28,14 @@ define([
             `--vx:${Math.round(_.vx)};--fy:${Math.round(_.vy)}}`
         )
         .join("\n");
+      // Update all positions
+      dom_process.define(`${id} styles`, {
+        element: "style",
+        attributes: {},
+        children: [css],
+      });
+
+      // style_node.innerHTML =
     };
 
     // Default forces (just for testing)
@@ -65,17 +73,12 @@ define([
 
   function main() {
     const root = document.getElementById("August-2020-space");
+
     const dom_process = dp.make_dom_process();
-    console.log(`dom_process`, dom_process);
     dom_process.mounted.next({ id: "root", element: root });
 
-    const some_operations = [
-      { type: "contains-text", text: "formidable nightmares" },
-      { type: "uses-element", name: "i" },
-    ];
-    const template = operations_to_template(some_operations);
-
-    dom_process.define("alien", {
+    /*
+    dom_process_1.define("alien", {
       element: "i",
       attributes: {},
       children: [
@@ -83,8 +86,8 @@ define([
         { element: "placeholder", attributes: { id: "spook" }, children: [] },
       ],
     });
-    dom_process.define("species", template);
-    dom_process.define("root", {
+    // dom_process_1.define("species", template);
+    dom_process_1.define("root1", {
       element: "b",
       attributes: {},
       children: [
@@ -94,11 +97,53 @@ define([
         "bar",
       ],
     });
+	*/
+    const names1 = "Alice Bob Carol".split(" ");
+    const names2 = "Dave Edie Frank".split(" ");
+    dom_process.define(
+      "root",
+      operations_to_template([
+        { type: "attribute-contains-word", name: "class", value: "panels" },
+        { type: "contains", id: "space1" },
+        { type: "contains", id: "space2" },
+      ])
+    );
+    dom_process.define(
+      "space1",
+      operations_to_template([
+        { type: "attribute-equals", name: "typeof", value: "Space" },
+        { type: "contains", id: `space1 styles` },
+        ...names1.map(id => ({ type: "contains", id })),
+      ])
+    );
+    dom_process.define(
+      "space2",
+      operations_to_template([
+        { type: "attribute-equals", name: "typeof", value: "Space" },
+        { type: "contains", id: `space2 styles` },
+        ...names2.map(id => ({ type: "contains", id })),
+      ])
+    );
+    for (const name of [...names1, ...names2])
+      dom_process.define(
+        name,
+        operations_to_template([
+          { type: "uses-element", name: "b" },
+          { type: "attribute-equals", name: "id", value: name },
+          {
+            type: "contains-text",
+            text: `My name is ${name.toUpperCase()} fool!`,
+          },
+        ])
+      );
 
-    const { streams } = make_space({ root, dom_process });
-    const { ticker, nodes, forces } = streams;
-    const EXAMPLE_NODES = [{ id: "Alice" }, { id: "Bob" }, { id: "Carol" }];
-    nodes.next(EXAMPLE_NODES);
+    const one = make_space({ dom_process, id: "space1" });
+    const two = make_space({ dom_process, id: "space2" });
+    // const { streams } = make_space({ root: root1, dom_process: dom_process_1 });
+    // const { ticker, nodes, forces } = streams;
+    const box_id = id => ({ id });
+    one.streams.nodes.next(names1.map(box_id));
+    two.streams.nodes.next(names2.map(box_id));
   }
 
   main();
