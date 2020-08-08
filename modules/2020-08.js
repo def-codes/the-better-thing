ba7b686140c8998e861b6f1ddb6b76e4632d42c3
Gavin Cannizzaro <gcannizzaro@mindgrub.com>
2020-08-07 23:01:52 -0400

    drizzle

--- modules/2020-08.js
+++ modules/2020-08.js
@@ -327,131 +327,144 @@ define([
     dom_process.mounted.next({ id: "world", element: root });
 
     const EXAMPLE = {
-      a: "Panel",
-      pane1: {
-        a: "Space",
-        ["d3:forces"]: DEFAULT_FORCES,
-        What: {},
-        Define: {},
-        Require: {},
+      a: ["Fullscreen", "Panel"],
+      alpha: {
+        a: ["Space"],
+        "d3:forces": { temp: { a: "d3:forceX", x: 250 } },
+        ball: { a: "Thing" },
       },
-      pane2: {
-        a: "Space",
-        ["d3:forces"]: DEFAULT_FORCES,
-        Sherry: { a: "Woman" },
-        Sue: { a: "Woman" },
-        dataflow: {
-          // What this would actually be.... not you writing out all of the things, but
-          // you're creating a view, a container, and you would describe the things that
-          // should be included in this space.  with various kinds of matching at your disposal
+      views: {
+        a: "Panel",
+        pane1: {
           a: "Space",
           ["d3:forces"]: {
-            charge: { a: "d3:forceManyBody" },
-            "x-axis": { a: "d3:forceX", x: 0, strength: 0.01 },
-            "y-axis": { a: "d3:forceY", y: 0, strength: 0.01 },
+            // View the default forces in a space using the default forces
+            // a: "Space",
+            // ["d3:forces"]: DEFAULT_FORCES,
+            ...DEFAULT_FORCES,
           },
-          Billy: { a: "Person" },
-          Nellie: { a: "Person" },
-          // things: {
-          //   a: "Space",
-          //   // a: "Collection",
-          //   sim1: { a: "Simulation" },
-          //   trace_me: { a: "Runner", x: { a: "Counter" }, y: {} },
-          // },
+          What: {},
+          Define: {},
+          Require: {},
         },
-        foo: {
-          a: "Space",
+        pane2: {
+          a: ["Table", "Space"],
           ["d3:forces"]: DEFAULT_FORCES,
-          forces: {
-            a: "Map",
-            charge: {
-              comment: "causes things to repel each other",
-              a: "ForceManyBody",
-              strength: 1, // "expression goeth here",
-              // Could be a constant expression
-              // Could be a source description
-              // Could be a function (of the node)
-              // Could be a source that emits functions?
-            },
-          },
+          Sherry: { a: "Woman" },
+          Sue: { a: "Woman" },
           dataflow: {
-            assertions: {
-              a: "StreamSync",
-              comment: "coordinate all dom assertions about representations",
-            },
-            [meld.dom_sink]: { a: "Sink" },
-            step: {
-              a: ["Source", "Counter"],
-              comment:
-                "the simulation does not schedule itself.  for best results, feed it at regular intervals",
-              transforms_with: [["map", () => sim.tick()]],
-            },
-            css: {
-              a: "Sink",
-              comment:
-                "map variable assignments from simulation nodes to css.  could be done via writing dom nodes with serialized style rules, or possibly by direct manipulation of host interfaces representing those rules.  I'm assuming there's some difference in overhead.",
-              listens_to: { id: "step" },
-              transforms_with: [
-                [
-                  "map",
-                  bodies =>
-                    bodies.map(
-                      _ =>
-                        css.rules(
-                          ...["x", "y", "vx", "vy"].map(
-                            v => [
-                              `[id="${_.id}"]`,
-                              `[data-${v}-source]`,
-                              { [`--${v}`]: _[v]?.toFixed(1) },
-                            ],
-                            []
-                          )
-                        ),
-                      // “Unrolled” version (which lumps all together)
-                      // Name might not be a legal ID
-                      css.rule([`[id="${_.id}"]`, "[]"], {
-                        "--x": _.x,
-                        "--y": _.y,
-                        "--vx": _.vx,
-                        "--vy": _.vy,
-                      })
-                    ),
-                ],
-              ],
-            },
-
-            // D3 has some of its own internal dataflow
-            // each time a force definition is updated
-            // the force values have to be recomputed
-          },
-        },
-        space1: {
-          // Space is a way of viewing something, not (always) the thing itself
-          a: "Space",
-          SomeGroup: {
+            // What this would actually be.... not you writing out all of the things, but
+            // you're creating a view, a container, and you would describe the things that
+            // should be included in this space.  with various kinds of matching at your disposal
             a: "Space",
-            Greg: { a: "Person" },
-            Jimbo: { a: "Person" },
-            Johnson: {
-              a: "Space",
-              Fred: { a: "Person" },
-              Bob: { a: "Person" },
+            ["d3:forces"]: {
+              charge: { a: "d3:forceManyBody" },
+              "x-axis": { a: "d3:forceX", x: 0, strength: 0.01 },
+              "y-axis": { a: "d3:forceY", y: 0, strength: 0.01 },
+            },
+            Billy: { a: "Person" },
+            Nellie: { a: "Person" },
+            // things: {
+            //   a: "Space",
+            //   // a: "Collection",
+            //   sim1: { a: "Simulation" },
+            //   trace_me: { a: "Runner", x: { a: "Counter" }, y: {} },
+            // },
+          },
+          foo: {
+            a: "Space",
+            ["d3:forces"]: DEFAULT_FORCES,
+            forces: {
+              a: "Map",
+              charge: {
+                comment: "causes things to repel each other",
+                a: "ForceManyBody",
+                strength: 1, // "expression goeth here",
+                // Could be a constant expression
+                // Could be a source description
+                // Could be a function (of the node)
+                // Could be a source that emits functions?
+              },
+            },
+            dataflow: {
+              assertions: {
+                a: "StreamSync",
+                comment: "coordinate all dom assertions about representations",
+              },
+              [meld.dom_sink]: { a: "Sink" },
+              step: {
+                a: ["Source", "Counter"],
+                comment:
+                  "the simulation does not schedule itself.  for best results, feed it at regular intervals",
+                transforms_with: [["map", () => sim.tick()]],
+              },
+              css: {
+                a: "Sink",
+                comment:
+                  "map variable assignments from simulation nodes to css.  could be done via writing dom nodes with serialized style rules, or possibly by direct manipulation of host interfaces representing those rules.  I'm assuming there's some difference in overhead.",
+                listens_to: { id: "step" },
+                transforms_with: [
+                  [
+                    "map",
+                    bodies =>
+                      bodies.map(
+                        _ =>
+                          css.rules(
+                            ...["x", "y", "vx", "vy"].map(
+                              v => [
+                                `[id="${_.id}"]`,
+                                `[data-${v}-source]`,
+                                { [`--${v}`]: _[v]?.toFixed(1) },
+                              ],
+                              []
+                            )
+                          ),
+                        // “Unrolled” version (which lumps all together)
+                        // Name might not be a legal ID
+                        css.rule([`[id="${_.id}"]`, "[]"], {
+                          "--x": _.x,
+                          "--y": _.y,
+                          "--vx": _.vx,
+                          "--vy": _.vy,
+                        })
+                      ),
+                  ],
+                ],
+              },
+
+              // D3 has some of its own internal dataflow
+              // each time a force definition is updated
+              // the force values have to be recomputed
             },
           },
-          Bob: { a: "Person" },
-          Carol: { a: "Person" },
-        },
-        space2: {
-          a: "Space",
-          Dave: { a: "Person" },
-          Edie: { a: "Person" },
-          Frank: { a: "Person" },
-        },
-        space3: {
-          a: "Space",
-          Joe: { a: "Person" },
-          Al: { a: "Person" },
-          Sue: { a: "Person" },
+          space1: {
+            // Space is a way of viewing something, not (always) the thing itself
+            a: "Space",
+            SomeGroup: {
+              a: "Space",
+              Greg: { a: "Person" },
+              Jimbo: { a: "Person" },
+              Johnson: {
+                a: "Space",
+                Fred: { a: "Person" },
+                Bob: { a: "Person" },
+              },
+            },
+            Bob: { a: "Person" },
+            Carol: { a: "Person" },
+          },
+          space2: {
+            a: "Space",
+            Dave: { a: "Person" },
+            Edie: { a: "Person" },
+            Frank: { a: "Person" },
+          },
+          space3: {
+            a: "Space",
+            Joe: { a: "Person" },
+            Al: { a: "Person" },
+            Sue: { a: "Person" },
+          },
         },
       },
     };
@@ -613,7 +626,28 @@ ${Object.entries(properties)
     });
 
     const mouse_moves = rs.fromEvent(document.body, "mousemove");
-    // mouse_moves.transform(tx.trace("fbalsdf"));
+    mouse_moves.transform(
+      tx.map(_ => {
+        console.log(_);
+        return {
+          type: "https://www.w3.org/TR/uievents/#mouseevent",
+          timestamp: _.timestamp,
+          x: _.clientX,
+          y: _.clientY,
+          movementX: _.movementX,
+          movementY: _.movementY,
+          button: _.button,
+          ctrlKey: _.ctrlKey,
+          shiftKey: _.shiftKey,
+          altKey: _.altKey,
+          metaKey: _.metaKey,
+        };
+      }),
+      tx.sideEffect(record => {
+        // assert this record into the graph
+        console.log("yeay", record);
+      })
+    );
   }
 
   main();
--- modules/examples/mouse-events.js
+++ modules/examples/mouse-events.js
@@ -2,7 +2,8 @@ define([], () => ({
   "@graph": [
     {
       "@id": "_:n1",
-      "@type": "https://dom.spec.whatwg.org/#interface-event",
+      // https://dom.spec.whatwg.org/#interface-event
+      "@type": "https://www.w3.org/TR/uievents/#mouseevent",
       "https://dom.spec.whatwg.org/#dom-event-timestamp": 1596590104934,
       // LS is in "draft status"
       // https://drafts.csswg.org/cssom-view/#dom-mouseevent-clientx
